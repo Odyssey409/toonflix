dd5fa4f92d8d5fa783257a9fb6963c0d5b8ec0b3
Odyssey409 <77610388+Odyssey409@users.noreply.github.com>
2024-09-25 00:48:56 +0900

    #3.2 Buttons Sections
    widget card의 잔고 표시 디자인 일부 완성. Container를 활용했는데, 이는 html의 div와 비슷한 역할을 하는 class임

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,11 @@ class App extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Color(0XFF181818),
         body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 40),
+          padding: EdgeInsets.symmetric(
+            horizontal: 40,
+          ),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 height: 80,
@@ -50,6 +53,50 @@ class App extends StatelessWidget {
                   )
                 ],
               ),
+              SizedBox(
+                height: 120,
+              ),
+              Text(
+                'Total Balance',
+                style: TextStyle(
+                  color: Colors.white.withOpacity(0.8),
+                  fontSize: 22,
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                '\$5 194 482',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 42,
+                    fontWeight: FontWeight.w600),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 15,
+                        horizontal: 40,
+                      ),
+                      child: Text('Transfer',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                          )),
+                    ),
+                  )
+                ],
+              )
             ],
           ),
         ),
