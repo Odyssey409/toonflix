55ffa5e7c46d52d2333286324ab429057d581dd0
Odyssey409 <77610388+Odyssey409@users.noreply.github.com>
2024-09-30 01:36:10 +0900

    불필요 요소 삭제
    코드 내 불필요 요소 삭제

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:toonflix/widgets/button.dart';
 import 'package:toonflix/widgets/currency_card.dart';
 
-class Player {
-  String? name;
-  Player({this.name});
-}
-
 void main() {
-  var odyssey = Player();
   runApp(const App());
 }
 
